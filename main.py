db944a1cef7fe0f75d12e8aa119d8448a1509566
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-05-11 17:38:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os, json
+
+base = {"type":"directory","content":{"soysoup":{"type":"directory","content":{},"home":{"type":"directory","content":{"downloads":{"type":"directory","content":{}},"documents":{"type":"directory","content":{}},"programs":{"type":"directory","content":{}}}}}}}
+
+programs = os.listdir("programs")
+
+for program in programs:
+    with open(f"programs/{program}","r",encoding="utf-8") as f:
+        data = f.read()
+
+    base["content"]["soysoup"]["content"][program.removesuffix(".js") + ".soup"] = {"type":"file","content":data}
+
+print(repr(json.dumps(base)))
